5c93bd3a7fdfcba54c702ef8a0d68dcb412ea4cb
Aimen Hammed <hammedaimennc@gmail.com>
2024-12-13 15:51:35 +0100

    Feat: Pouvoir confronter quelqu'un en duel

--- src/main/java/dwn/cda/thebot/bot/Bot.java
+++ src/main/java/dwn/cda/thebot/bot/Bot.java
@@ -1,9 +1,11 @@
 package dwn.cda.thebot.bot;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
@@ -15,18 +17,24 @@ public class Bot extends ListenerAdapter {
     public void onGuildReady(@NotNull GuildReadyEvent event) {
         guild = event.getGuild();
         guild.updateCommands().addCommands(
-                Commands.slash("hello", "Say Hello")
+                Commands.slash("duel", "Provoquer un utilisateur en duel")
+                        .addOption(OptionType.USER, "user", "user", true)
         ).queue();
     }
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         switch (event.getName()) {
-            case "hello":
-                event.reply("Hello World").queue();
+            case "duel":
+                Member adversaire = event.getOption("user").getAsMember();
+                if(adversaire != null)
+                event.reply(event.getUser().getAsMention()+ " défie " +adversaire.getAsMention()+ " en duel !").queue();
+                else {
+                    event.reply("Utilisateur inconnu").setEphemeral(true).queue();
+                }
                 break;
             default:
-                event.reply("I'm a teapot").setEphemeral(true).queue();
+                event.reply("Requete inconnu").setEphemeral(true).queue();
         }
     }
 }
